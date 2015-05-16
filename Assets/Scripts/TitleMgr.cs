52d06b67f0d4e5b522764953bf199e558af58842
hogehogeman <mail@ktx.jp>
2015-05-16 21:04:18 +0900

    First commit 150516

---
+++ Assets/Scripts/TitleMgr.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMgr : MonoBehaviour {
+
+	void OnGUI()
+	{
+		Util.SetFontSize (32);
+		Util.SetFontAlignment (TextAnchor.MiddleCenter);
+		Util.SetFontColor(Color.yellow);
+
+		float w = 128;
+		float h = 32;
+		float px = Screen.width / 2 - w / 2;
+		float py = Screen.height / 2 - h / 2;
+
+		Util.GUILabel (px, py, w, h, "SOGA HEAD");
+
+
+		py += 60;
+
+		if(GUI.Button (new Rect(px,py,w,h), "START"))
+		{
+			Application.LoadLevel("Main");
+		}
+	}
+}
